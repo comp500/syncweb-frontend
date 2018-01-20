634a4004f2a306997bcd0aad6e38b803dd3228e1
comp500 <comp500@users.noreply.github.com>
2018-01-20 17:43:24 +0000

    id function, to make code more concise

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,27 @@
+let id = document.getElementById.bind(document);
+
 if (!window.addEventListener) {
-	document.getElementById("compat-errors").innerHTML = "Your browser does not support features (addEventListener) required for SyncWeb. Please update to a newer browser.";
+	id("compat-errors").innerHTML = "Your browser does not support features (addEventListener) required for SyncWeb. Please update to a newer browser.";
 }
 
 if (!window.WebSocket) {
-	document.getElementById("compat-errors").innerHTML = "Your browser does not support features (WebSocket) required for SyncWeb. Please update to a newer browser.";
+	id("compat-errors").innerHTML = "Your browser does not support features (WebSocket) required for SyncWeb. Please update to a newer browser.";
 }
 
 const syncWeb = new SyncWeb.Client();
 // remove when in production?
 window.syncWeb = syncWeb;
 
-document.getElementById("connection-form").addEventListener("submit", (e) => {
+id("connection-form").addEventListener("submit", (e) => {
 	e.preventDefault();
-	document.getElementById("connection-errors").innerHTML = "";
+	id("connection-errors").innerHTML = "";
 
-	let url = document.getElementById("url-input").value;
+	let url = id("url-input").value;
 	if (url != null && url.length > 3 && url.split("://").length == 2) {
 		// TODO: error notification
 		syncWeb.connect("WebSocket-builtin", { url });
 	} else {
-		document.getElementById("connection-errors").innerHTML = "Invalid URL.";
+		id("connection-errors").innerHTML = "Invalid URL.";
 	}
 
 	return false;
