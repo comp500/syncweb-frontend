5360e76c1779116ed21f22664b1ac11c4792bee6
comp500 <comp500@users.noreply.github.com>
2018-01-20 17:27:35 +0000

    implement on event

--- src/js/players/HTTPPlayer.js
+++ src/js/players/HTTPPlayer.js
@@ -8,6 +8,10 @@ class HTMLPlayer extends SyncWeb.Player {
 		return split[0] == "http";
 	}
 
+	on(event, data) {
+		console.log(event, data); // eslint-disable-line no-console
+	}
+
 	command(command, data) {
 		console.log(command, data); // eslint-disable-line no-console
 	}
