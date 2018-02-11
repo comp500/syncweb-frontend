ad13650742c4dd0eafa60f8ed612e98ee0a44823
comp500 <comp500@users.noreply.github.com>
2018-02-11 21:09:28 +0000

    Fix connection messages

--- src/js/UIProxy.js
+++ src/js/UIProxy.js
@@ -4,10 +4,22 @@ class UIProxy extends SyncWeb.PlayerProxy {
 	}
 
 	event(event, data) {
+		let id = document.getElementById.bind(document);
 		console.log("uiproxy", event, data); // eslint-disable-line no-console
 		switch (event) {
+			case "connecting":
+				id("syncweb-connconfig").classList.add("hidden");
+				id("syncweb-player").innerText = "Connecting with protocol " + data;
+				break;
+			case "connected":
+				if (data) {
+					id("syncweb-player").innerText = data;
+				} else {
+					id("syncweb-player").innerText = "Connected to socket, loading...";
+				}
+				break;
 			case "roomdetails":
-				
+
 				break;
 		}
 	}
