0549bb910bc0294c24c816152cc1c3073561b28b
comp500 <comp500@users.noreply.github.com>
2019-09-05 20:24:20 +0100

    Update to new api, reformat

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,11 +1,13 @@
 let id = document.getElementById.bind(document);
 
 if (!window.addEventListener) {
-	id("compat-errors").innerHTML = "Your browser does not support features (addEventListener) required for SyncWeb. Please update to a newer browser.";
+	id("compat-errors").innerHTML =
+		"Your browser does not support features (addEventListener) required for SyncWeb. Please update to a newer browser.";
 }
 
 if (!WebSocket) {
-	id("compat-errors").innerHTML = "Your browser does not support features (WebSocket) required for SyncWeb. Please update to a newer browser.";
+	id("compat-errors").innerHTML =
+		"Your browser does not support features (WebSocket) required for SyncWeb. Please update to a newer browser.";
 }
 
 import { SyncplayJSONClient } from "syncweb-js/src/index";
@@ -15,43 +17,52 @@ const syncWeb = new SyncplayJSONClient();
 // TODO make this detachable from player, and not created on startup, for changeable protocols
 const currentPlayer = new HTTPPlayer(id("syncweb-player"));
 
-id("connection-form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	id("connection-errors").innerHTML = "";
+id("connection-form").addEventListener(
+	"submit",
+	e => {
+		e.preventDefault();
+		id("connection-errors").innerHTML = "";
 
-	let url = id("url-input").value;
-	if (url != null && url.length > 3 && url.split("://").length == 2) {
-		// TODO: error notification
-		let name = id("name-input").value;
-		let room = id("room-input").value;
-		if (name != null && name.length > 0) {
-			if (room != null && room.length > 0) {
-				syncWeb.connect({url, name, room}, () => {
-					id("syncweb-connconfig").classList.add("hidden");
-					id("syncweb-player").innerText = "Connected to socket, loading...";
-				});
+		let url = id("url-input").value;
+		if (url != null && url.length > 3 && url.split("://").length == 2) {
+			// TODO: error notification
+			let name = id("name-input").value;
+			let room = id("room-input").value;
+			if (name != null && name.length > 0) {
+				if (room != null && room.length > 0) {
+					syncWeb.connected.subscribe((motd, version, features) => {
+						id("syncweb-connconfig").classList.add("hidden");
+						id("syncweb-player").innerText = "Connected to socket, loading...";
+					});
+					syncWeb.connect(name, room, url);
+				} else {
+					id("connection-errors").innerHTML = "Please type a room.";
+				}
 			} else {
-				id("connection-errors").innerHTML = "Please type a room.";
+				id("connection-errors").innerHTML = "Please type a username.";
 			}
 		} else {
-			id("connection-errors").innerHTML = "Please type a username.";
+			id("connection-errors").innerHTML = "Invalid URL.";
 		}
-	} else {
-		id("connection-errors").innerHTML = "Invalid URL.";
-	}
 
-	return false;
-}, true);
+		return false;
+	},
+	true
+);
 
-id("httpvideo-form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	id("connection-errors").innerHTML = "";
+id("httpvideo-form").addEventListener(
+	"submit",
+	e => {
+		e.preventDefault();
+		id("connection-errors").innerHTML = "";
 
-	let url = id("httpvideo-input").value;
-	currentPlayer.setURL(url);
+		let url = id("httpvideo-input").value;
+		currentPlayer.setURL(url);
 
-	return false;
-}, true);
+		return false;
+	},
+	true
+);
 
 let appendChat = (message, name?) => {
 	let domMsg = document.createElement("div");
@@ -67,7 +78,7 @@ let appendChat = (message, name?) => {
 };
 
 // TODO make these detachable from protocol, for changeable protocols
-syncWeb.connected.subscribe((data) => {
+syncWeb.connected.subscribe(data => {
 	if (data) {
 		id("syncweb-player").innerText = data;
 		appendChat(data);
@@ -76,50 +87,50 @@ syncWeb.connected.subscribe((data) => {
 	}
 });
 
-syncWeb.roomDetailsUpdated.subscribe((data) => {
+syncWeb.usersUpdated.subscribe(data => {
 	appendChat(JSON.stringify(data), "room details");
 });
 
-syncWeb.chat.subscribe((name, message) => {
-	appendChat(message, name);
+syncWeb.chat.subscribe((user, message) => {
+	appendChat(message, user.name);
 });
 
-syncWeb.joined.subscribe((user, room) => {
-	appendChat(`${user} joined room: "${room}"`);
+syncWeb.joined.subscribe(user => {
+	appendChat(`${user.name} joined room: "${user.room}"`);
 });
 
-syncWeb.left.subscribe((user, room) => {
-	appendChat(`${user} left room: "${room}"`);
+syncWeb.left.subscribe(user => {
+	appendChat(`${user.name} left room: "${user.room}"`);
 });
 
 syncWeb.moved.subscribe((user, room) => {
-	appendChat(`${user} moved to room: "${room}"`);
+	appendChat(`${user.name} moved to room: "${user.room}" from: "${room}"`);
 });
 
 syncWeb.seek.subscribe((position, user) => {
-	appendChat(`${user} seeked to ${position}`);
+	appendChat(`${user.name} seeked to ${position}`);
 	currentPlayer.seekTo(position);
 });
 
-syncWeb.pause.subscribe((user) => {
-	appendChat(`${user} paused`);
+syncWeb.pause.subscribe(user => {
+	appendChat(`${user.name} paused`);
 	currentPlayer.pause();
 });
 
-syncWeb.unpause.subscribe((user) => {
+syncWeb.unpause.subscribe(user => {
 	// potential problem: unpause is sent from video.play()
 	// could result in unintentional ready setting
-	appendChat(`${user} unpaused`);
+	appendChat(`${user.name} unpaused`);
 	currentPlayer.play();
 });
 
-currentPlayer.setTime.subscribe((position) => {
+currentPlayer.setTime.subscribe(position => {
 	syncWeb.setTime(position);
 });
 
-currentPlayer.seeked.subscribe((position) => {
-	if (syncWeb.getCurrentUsername()) {
-		appendChat(`${syncWeb.getCurrentUsername()} seeked to ${position}`);
+currentPlayer.seeked.subscribe(position => {
+	if (syncWeb.getUsername()) {
+		appendChat(`${syncWeb.getUsername()} seeked to ${position}`);
 	} else {
 		appendChat(`Seeked to ${position}`);
 	}
@@ -127,8 +138,8 @@ currentPlayer.seeked.subscribe((position) => {
 });
 
 currentPlayer.unpaused.subscribe(() => {
-	if (syncWeb.getCurrentUsername()) {
-		appendChat(`${syncWeb.getCurrentUsername()} unpaused`);
+	if (syncWeb.getUsername()) {
+		appendChat(`${syncWeb.getUsername()} unpaused`);
 	} else {
 		appendChat(`Unpaused`);
 	}
@@ -136,8 +147,8 @@ currentPlayer.unpaused.subscribe(() => {
 });
 
 currentPlayer.paused.subscribe(() => {
-	if (syncWeb.getCurrentUsername()) {
-		appendChat(`${syncWeb.getCurrentUsername()} paused`);
+	if (syncWeb.getUsername()) {
+		appendChat(`${syncWeb.getUsername()} paused`);
 	} else {
 		appendChat(`Paused`);
 	}
@@ -145,5 +156,5 @@ currentPlayer.paused.subscribe(() => {
 });
 
 currentPlayer.setMeta.subscribe((name, duration) => {
-	syncWeb.sendFile(duration, name);
+	syncWeb.sendFile(name, duration);
 });
